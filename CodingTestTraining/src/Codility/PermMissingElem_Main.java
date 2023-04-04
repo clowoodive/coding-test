607440d250ca4903d96a1370aa039dd1c61442f8
clowoodive <clowoodive@gmail.com>
2023-04-05 02:41:16 +0900

    Feat: 풀이 4종 추가

---
+++ CodingTestTraining/src/Codility/PermMissingElem_Main.java
@@ -0,0 +1,28 @@
+package Codility;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PermMissingElem_Main {
+    public static void main(String[] args) {
+        int solution = solution(new int[]{2, 3, 1, 5});
+        System.out.println(solution);
+    }
+
+    private static int solution(int[] A) {
+        // 순차 값 포함된 array에서 누락된 값 찾기
+        // 효율 고려
+
+        Set<Integer> allNumSet = new HashSet<>();
+
+        for (int i = 1; i <= A.length + 1; i++) {
+            allNumSet.add(i);
+        }
+
+        for (var v : A) {
+            allNumSet.remove(v);
+        }
+
+        return allNumSet.stream().findAny().orElse(0);
+    }
+}
